262fedc575009f6bee61fb72b1b9b97bb80725b6
Iamneo <techadmin@iamneo.ai>
2023-09-01 06:21:34 +0000

    Backup Commit

--- Apptest.java
+++ Apptest.java
@@ -11,35 +11,37 @@ public class Apptest {
       WebDriver driver;
       String fbUrl = "https://www.fb.com";
       String facebookUrl = "https://www.facebook.com";
-@BeforeClass
-public void invokeBrowser() {
-System.setProperty("webdriver.chrome.driver",
-"C:/Users/Ritika/workspace/libs/chromedriver_76/Chromedriver.exe");  
-driver = new ChromeDriver();
-driver.manage().window().maximize();
-driver.manage().deleteAllCookies();
-driver.get(fbUrl);
-String urlFromBrowser = driver.getCurrentUrl();
-Assert.assertEquals(urlFromBrowser, facebookUrl, "No redirection happened");
-}
-@Test
-public void facebookSignUp() {
-driver.findElement(By.name("firstname")).sendKeys("Test");
-driver.findElement(By.name("lastname")).sendKeys("User");
-driver.findElement(By.name("reg_email__")).sendKeys("testuser@test.com");
-driver.findElement(By.name("reg_passwd__")).sendKeys("testPassword");
-Select selDate = new Select(driver.findElement(By.id("day")));
-Select selMonth = new Select(driver.findElement(By.id("month")));
-Select selYear = new Select(driver.findElement(By.id("year")));
-selDate.selectByVisibleText("21");
-selMonth.selectByVisibleText("Jun");
-selYear.selectByVisibleText("1989");
-driver.findElement(By.xpath("//input[@type='radio' and @value='2']")).click();
-driver.findElement(By.xpath("//button[text()='Sign Up']")).click();
-}
+      @BeforeClass
+    public void invokeBrowser() {
+        System.setProperty("webdriver.chrome.driver",
+            "C:/Users/Ritika/workspace/libs/chromedriver_76/Chromedriver.exe");  
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+        driver.get(fbUrl);
+        String urlFromBrowser = driver.getCurrentUrl();
+        Assert.assertEquals(urlFromBrowser, facebookUrl, "No redirection happened");
+    } 
+     
+    @Test
+    public void facebookSignUp() {
+        driver.findElement(By.name("firstname")).sendKeys("Test");
+        driver.findElement(By.name("lastname")).sendKeys("User");
+        driver.findElement(By.name("reg_email__")).sendKeys("testuser@test.com");
+        driver.findElement(By.name("reg_passwd__")).sendKeys("testPassword");
+        Select selDate = new Select(driver.findElement(By.id("day")));
+        Select selMonth = new Select(driver.findElement(By.id("month")));
+        Select selYear = new Select(driver.findElement(By.id("year")));
+        selDate.selectByVisibleText("21");
+        selMonth.selectByVisibleText("Jun");
+        selYear.selectByVisibleText("1989");
+        driver.findElement(By.xpath("//input[@type='radio' and @value='2']")).click();
+        driver.findElement(By.xpath("//button[text()='Sign Up']")).click();
+    }
 @AfterClass
 public void closeBrowser() {
-driver.quit();
+        driver.quit();
+}
 }
 }
 
